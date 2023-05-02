cc93eacce2c69dc568058b2c0aba0269434e4ac4
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 18:37:43 -0400

    get  all comment data

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,3 +2,20 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll({});
+      if (commentData.length === 0) {
+        res
+          .status(404)
+          .json({ message: 'would you believe there are NO comments ANYWHERE?' });
+        return;
+      }
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  
