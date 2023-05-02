153ee49617c1b3e72e2855cc1e0a6c9bdc710eba
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 20:48:44 -0400

    use routes response

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,3 +7,8 @@ const dashboardRoutes = require('./dashboard-routes.js');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+  });
+  
