08090f28005ebcb396d1651d5b5615c37bfccdc7
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:26:18 -0400

    object query selector

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,5 @@
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
