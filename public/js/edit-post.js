70c460a454a23c4a794cbdcc3b8047750360de67
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:35:47 -0400

    response update

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -7,4 +7,16 @@ async function editFormHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
+      
+    const response = await fetch(`/api/post/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id,
+          title,
+          content,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     
