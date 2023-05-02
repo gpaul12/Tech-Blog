4ed914dd293424b48cb68deada85eece0fd3d160
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 21:09:54 -0400

    post login auth

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -64,3 +64,33 @@ router.get('/', async (req, res) => {
     }
   });
   
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({
+        where: { username: req.body.username },
+      });
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: `${req.body.username} is not a valid username` });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username;
+        req.session.logged_in = true;
+  
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
