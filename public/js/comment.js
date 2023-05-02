70ef1bbaa3ffc2d299a65930dcef97e76edd8c78
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:42:55 -0400

    if statement for comment text reponse

--- public/js/comment.js
+++ public/js/comment.js
@@ -8,3 +8,15 @@ async function commentFormHandler(event) {
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
+
+      if (comment_text) {
+        const response = await fetch('/api/comment', {
+          method: 'POST',
+          body: JSON.stringify({
+            post_id,
+            comment_text,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
