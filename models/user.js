4075699834f3107f8f141708893e536ec20d69eb
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 18:50:47 -0400

    user email

--- models/user.js
+++ models/user.js
@@ -21,4 +21,11 @@ class User extends Model {
         allowNull: false,
         unique: true,
       },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          isEmail: true,
+        },
+      },
       
