e3869f6878a0da5ad1ea8a5d40d790d7d1693d45
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:27:44 -0400

    if statement for username and passwrd

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,3 +3,20 @@ async function signupFormHandler(event) {
 
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/user', {
+          method: 'POST',
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    }
