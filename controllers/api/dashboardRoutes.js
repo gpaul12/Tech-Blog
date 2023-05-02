29080624d6e137ffce5515c69a9ec44badc9cd7f
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 21:00:23 -0400

    get model info by id

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -38,4 +38,25 @@ router.get('/', withAuth, (req, res) => {
       });
   });
 
-  
+  router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'title', 'content', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+      ],
+    })
+    
