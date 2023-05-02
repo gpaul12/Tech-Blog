bf4ed0f56d3362ac01597753afb59f2e316c6654
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 18:46:41 -0400

    post id

--- models/post.js
+++ models/post.js
@@ -3,3 +3,11 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
