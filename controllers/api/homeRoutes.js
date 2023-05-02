81dba53c38cc0470c7fb2cb762ea0b7ca6d3831a
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 20:56:16 -0400

    get post comments

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -85,4 +85,25 @@ router.get('/', (req, res) => {
       });
   });
 
-  
+  router.get('/posts-comments', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'content', 'title', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    })
+    
