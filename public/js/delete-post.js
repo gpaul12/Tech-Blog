ab1e3e169654569113ed6209fcb568e3a101b901
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:38:48 -0400

    delete response

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -4,3 +4,13 @@ async function deleteFormHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
+
+    const response = await fetch(`/api/post/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+          post_id: id,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    });
