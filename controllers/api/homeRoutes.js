eebe1ee84818fe02598896357798101b6fd58767
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 20:53:12 -0400

    get model info

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,3 +1,23 @@
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: ['id', 'title', 'content', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    })
+    
