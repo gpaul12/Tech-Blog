28f0be9cb90c91dc00b68032d4dffe4e0546dc12
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:45:27 -0400

    get response from post

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -3,3 +3,14 @@ async function newFormHandler(event) {
 
     const title = document.querySelector('input[name="post-title"]').value;
     const content = document.querySelector('input[name="content"]').value;
+
+    const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          content,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
