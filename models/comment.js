5821e7a4021ffc568849bf364d05d37868a20a0f
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 18:43:16 -0400

    comment id

--- models/comment.js
+++ models/comment.js
@@ -2,3 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
+
+Comment.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
