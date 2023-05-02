fa8120d2e7a434a3bf5883684c12ae23e2cec24a
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 18:38:08 -0400

    get comment data by id

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -18,4 +18,22 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/:id', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll({
+        where: { id: req.params.id },
+      });
+      if (commentData.length === 0) {
+        res
+          .status(404)
+          .json({ message: `There is no comment with id = ${req.params.id}` });
+        return;
+      }
+      // res.status(200).json(commentData.reverse());
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
   
