8c04da072d6d1ae26232e53d9ccfbbeb6067dc5c
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 21:07:09 -0400

    get userdata

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  
