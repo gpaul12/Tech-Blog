893bfcef0260929f8106963bb1024fd8aec6ae16
Gerry Paul <gpaul12@nc.rr.com>
2023-05-02 19:34:36 -0400

    object values

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,2 +1,5 @@
 async function editFormHandler(event) {
     event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const content = document.querySelector('input[name="content"]').value.trim();
