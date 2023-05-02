d1db01a75020cf2b4442ab85d3ab5a625e0b957b
Gerry Paul <gpaul12@nc.rr.com>
2023-05-01 21:07:59 -0400

    get user by id

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,4 +12,39 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/:id', async (req, res) => {
+    try {
+      const userData = await User.findOne({
+        attributes: { exclude: ['password'] },
+        where: { id: req.params.id },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at'],
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title'],
+            },
+          },
+          {
+            model: Post,
+            attributes: ['title'],
+          },
+        ],
+      });
+      console.log(userData);
+      if (!userData) {
+        res.status(404).json({ message: `No such user id ${req.params.id}` });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
   
